4de79cadfd464a79d8489c35f84099fbb4ea30d3
Julian Mendoza <julianmendoza@Julians-MacBook-Air.local>
2024-05-02 11:11:29 -0500

    Start of project

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';  //If you need Authentication
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl9_kB-2ATNMqB6GTVbyVQ8d6KuYAhY8s",
-  authDomain: "todocalendar-b0a6f.firebaseapp.com",
-  projectId: "todocalendar-b0a6f",
-  storageBucket: "todocalendar-b0a6f.appspot.com",
-  messagingSenderId: "122393275536",
-  appId: "1:122393275536:web:d659ff12b0fdc8a910fc15",
-  measurementId: "G-DR6CKH9BJ9"
+    apiKey: "AIzaSyBl9_kB-2ATNMqB6GTVbyVQ8d6KuYAhY8s",
+    authDomain: "todocalendar-b0a6f.firebaseapp.com",
+    projectId: "todocalendar-b0a6f",
+    storageBucket: "todocalendar-b0a6f.appspot.com",
+    messagingSenderId: "122393275536",
+    appId: "1:122393275536:web:d659ff12b0fdc8a910fc15",
+    measurementId: "G-DR6CKH9BJ9"
 };
 
 const app = initializeApp(firebaseConfig);
